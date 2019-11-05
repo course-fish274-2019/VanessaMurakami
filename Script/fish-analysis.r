d9e7f1c90dbbcba47cac4eaea3d26c493aa15c03
Vanessa Murakami <vanessamurakami@Vanessas-MacBook.local>
2019-11-05 10:49:29 -0800

    Created a scatterplot looking at fish length (X), and scalelength (Y)

--- Script/fish-analysis.r
+++ Script/fish-analysis.r
@@ -15,7 +15,18 @@ fish_data_cat = fish_data %>%
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
-
+#Question 4
 #Use dplyr to remove the fish with a scalelength of less than 1 mm from fish_data_cat. The new dataset will have 4,029 rows.
 
 fish_data_cat<-filter(fish_data_cat, scalelength>1)
+fish_data_cat
+
+
+#Question 5
+#To look at the relationship between the length of each fish’s body and the size of its scale across the different lakes sampled in these data, create a scatterplot with length on the x-axis and scalelength on the y-axis, then color the points using lakeid.
+
+library(tidyverse)
+
+ggplot(fish_data_cat,aes(x=length,y=scalelength,color=lakeid))+
+  geom_point(size=3)
+  
