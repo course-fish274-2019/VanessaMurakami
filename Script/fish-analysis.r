1343ee637dece041ff37937255af1486463708ef
Vanessa Murakami <vanessamurakami@Vanessas-MacBook.local>
2019-11-05 10:12:42 -0800

    Change size cutoff for new column

--- Script/fish-analysis.r
+++ Script/fish-analysis.r
@@ -8,3 +8,5 @@ library(dplyr)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 
+fish_data_cat = fish_data %>%
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
