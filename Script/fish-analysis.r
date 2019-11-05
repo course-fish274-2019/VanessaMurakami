823e4f0464c390e5659bd1fb29138122269d7189
Vanessa Murakami <vanessamurakami@Vanessas-MacBook.local>
2019-11-05 10:23:31 -0800

    Comment added explaining what the code does to the fish data and how fish_data_cat was made

--- Script/fish-analysis.r
+++ Script/fish-analysis.r
@@ -5,6 +5,10 @@ fish_data=read.csv("Data/Gaeta_etal_CLC_data.csv")
 
 
 library(dplyr)
+
+#The code below changes the fish data, where the first code adds categorical fish length column
+#The second code changes the size cutoff for the new column that was created
+
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 
