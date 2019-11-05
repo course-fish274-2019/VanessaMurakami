5d90538000eb3845f81731b21d958fb34efd0efc
Vanessa Murakami <vanessamurakami@Vanessas-MacBook.local>
2019-11-05 10:09:38 -0800

    Add categorical fish length column

--- Script/fish-analysis.r
+++ Script/fish-analysis.r
@@ -2,3 +2,9 @@
 #Created 5 November 2019 by Vanessa Murakami, vanmurak@uw.edu
 
 fish_data=read.csv("Data/Gaeta_etal_CLC_data.csv")
+
+
+library(dplyr)
+fish_data_cat = fish_data %>%
+  mutate(length_cat = ifelse(length > 200, "big", "small"))
+
